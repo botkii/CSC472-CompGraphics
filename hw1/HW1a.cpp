ffceda354318ea6ff7fe58658cce96fd36ecba4b
botkii <kennw902@gmail.com>
2025-09-08 11:00:05 -0400

    Add project files.

---
+++ hw1/HW1a.cpp
@@ -0,0 +1,147 @@
+// ===============================================================
+// Computer Graphics Homework Solutions
+// Copyright (C) 2022 by George Wolberg
+//
+// HW1a.cpp - HW1a class
+//
+// Written by: George Wolberg, 2022
+// ===============================================================
+
+#include "HW1a.h"
+#include <iostream>
+
+// init array of 16 vertices for letter 'P'
+float Vertices[] = {
+	-0.5f , -0.75f,
+	-0.5f , -0.5f,
+	-0.5f , -0.25f,
+	-0.5f ,  0.0f,
+	-0.5f ,  0.25f,
+	-0.5f ,  0.5f,
+	-0.25f,  0.75f,
+	 0.0f ,  0.75f,
+	 0.25f,  0.75f,
+	 0.5f ,  0.75f,
+	 0.75f,  0.5f,
+	 0.75f,  0.25f,
+	 0.5f ,  0.0f,
+	 0.25f,  0.0f,
+	 0.0f ,  0.0f,
+	-0.25f,  0.0f
+};
+
+int len = sizeof(Vertices);
+
+static int DrawModes[] = {
+	GL_POINTS,
+	GL_LINES,
+	GL_LINE_STRIP,
+	GL_LINE_LOOP,
+	GL_TRIANGLES,
+	GL_TRIANGLE_STRIP,
+	GL_TRIANGLE_FAN,
+	GL_QUADS,
+	GL_POLYGON
+};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// HW1a::HW1a:
+//
+// HW1a constructor.
+//
+HW1a::HW1a(const QGLFormat &glf, QWidget *parent)
+	: HW(glf, parent)
+{
+}
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// HW1a::initializeGL:
+//
+// Initialization routine before display loop.
+// Gets called once before the first time resizeGL() or paintGL() is called.
+//
+void
+HW1a::initializeGL()
+{
+	// PUT YOUR CODE HERE
+
+	// initialize GL function resolution for current context
+	initializeGLFunctions();
+
+	// init state variables
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);	// set background color
+	glColor3f(1.0F, 1.0f, 1.0f);		// set foreground color
+}
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// HW1a::resizeGL:
+//
+// Resize event handler.
+// The input parameters are the window width (w) and height (h).
+//
+void
+HW1a::resizeGL(int w, int h)
+{
+	// PUT YOUR CODE HERE
+	// compute aspect ratio
+	float xmax, ymax;
+	float ar = (float)w / h;
+	if (ar > 1.0) {		// wide screen
+		xmax = ar;
+		ymax = 1.;
+	}
+	else {		// tall screen
+		xmax = 1;
+		ymax = 1 / ar;
+	}
+
+	// set viewport to occupy full canvas
+	glViewport(0, 0, w, h);
+
+	// init viewing coordinates for orthographic projection
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glOrtho(-xmax, xmax, -ymax, ymax, -1.0, 1.0);
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// HW1a::paintGL:
+//
+// Update GL scene.
+//
+void
+HW1a::paintGL()
+{
+	int i;
+	// PUT YOUR CODE HERE
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_LINES);
+	for (i = 0; i < len; i+=2) {
+		glVertex2f(Vertices[i], Vertices[i + 1]);
+		glVertex2f(Vertices[i + 2], Vertices[i + 3]);
+		}
+	glEnd();
+
+}
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// HW1a::controlPanel:
+//
+// Create control panel groupbox.
+//
+QGroupBox*
+HW1a::controlPanel()
+{
+	// init group box
+	QGroupBox *groupBox = new QGroupBox("Homework 1a");
+	groupBox->setStyleSheet(GroupBoxStyle);
+
+	return(groupBox);
+}
