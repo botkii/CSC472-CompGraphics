86a9a2106af3b2a3f5b3ff59f00450cdfe99a248
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2025-11-13 21:18:15 -0500

    Implement vertex shader transformation

--- hw3/vshader3b2.glsl
+++ hw3/vshader3b2.glsl
@@ -1,8 +1,11 @@
 #version 330
 
-// PUT YOUR CODE HERE
+in vec3 a_Position;	// attribute variable: position vector
+
+uniform mat4 u_View;		// uniform variable for view matrix
+uniform mat4 u_Projection;	// uniform variable for projection matrix
 
 void main() 
 {
-	// PUT YOUR CODE HERE
+	gl_Position = u_Projection * u_View * vec4(a_Position, 1.0);
 }
